c60ae28fe891dddf1207f13cd66ad9295f3b5d50
Chizihn <chizihn@gmail.com>
2025-03-27 10:18:00 +0100

    fixd jwt

--- src/utils/token.ts
+++ src/utils/token.ts
@@ -11,7 +11,7 @@ export const generateToken = (userId: string, res: Response) => {
     maxAge: 7 * 24 * 60 * 60 * 1000,
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    sameSite: "strict",
+    sameSite: false,
   });
 
   return token;
